df448860dcac49b5cab4904c6f33c48e6b5b39ea
lightslash <chambostpol@gmail.com>
2019-02-22 23:25:29 +0100

    Commit du projet

--- restore_from_backup.sh
+++ restore_from_backup.sh
@@ -10,38 +10,38 @@ host=$1
 snapshot_name=$2
 
 how_to_use() {
-    echo "how to use: port user@hostname snapshot_name"
+    echo "how to use: port user@hostname snapshot_name \n"
     exit 1
 }
 
 restore_from_backup() {
-    echo "Mise en maintenance du serveur NextCloud"
+    echo "Mise en maintenance du serveur NextCloud \n"
     ssh -p $port $host "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on"
 
-    echo "Verrouillage snapshot"
+    echo "Verrouillage snapshot \n"
     zfs hold keep host
 
-    echo "Clonage snapshot"
+    echo "Clonage snapshot \n"
     zfs clone host data/restore
 
-    echo "Restauration des fichiers, veuillez patienter"
+    echo "Restauration des fichiers, veuillez patienter \n"
     rsync -Aavx /data/restore/nextcloud-dirbackup/ -e "ssh -p $port" $host:/var/www/html/nextcloud/
 
-    echo "Nettoyage de la base de données"
+    echo "Nettoyage de la base de données \n"
     ssh -p $port $host  "mysql -h $DATABASE_HOST -u $DATABASE_USER --password=$DATABASE_PASSWORD -e \"DROP DATABASE $DATABASE_DBNAME\""
     ssh -p $port $host  "mysql -h $DATABASE_HOST -u $DATABASE_USER --password=$DATABASE_PASSWORD -e \"CREATE DATABASE $DATABASE_DBNAME\""
 
-    echo "Restauration de la base de données, veuillez patienter"
+    echo "Restauration de la base de données, veuillez patienter \n"
     ssh -p $port $host  "mysql -h $DATABASE_HOST -u $DATABASE_USER --password=$DATABASE_PASSWORD $MYSQL_DBNAME" < /data/restore/nextcloud-dbbackup.bak
 
-    echo "Suppression du snapshot clone"
+    echo "Suppression du snapshot clone \n"
     zfs destroy data/restore
 
-    echo "Déverrouillage du snapshot"
+    echo "Déverrouillage du snapshot \n"
     zfs release keep host
 
     ssh -p $port $host "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off"
-    echo "Le serveur NextCloud est de nouveau opérationnel"
+    echo "Le serveur NextCloud est de nouveau opérationnel \n"
 
     exit 0
 }
@@ -53,10 +53,10 @@ then
 elif [[ $# -eq 1 ]];
     then
     snapshot_name=`zfs list -H -t snapshot -o name -S creation | head -1`
-    echo "Restauration du dernier snapshot $snapshot_name"
+    echo "Restauration du dernier snapshot $snapshot_name \n"
     restore_from_backup $snapshot_name
 elif [[ $# -eq 2 ]];
     then
-    echo "Restauration du snapshot $snapshot_name"
+    echo "Restauration du snapshot $snapshot_name \n"
     restore_from_backup $snapshot_name
 fi
--- save_from_backup.sh
+++ save_from_backup.sh
@@ -10,21 +10,21 @@ host=$1
 
 how_to_use() {
 
-    echo "how to use: port user@hostname"
+    echo "how to use: port user@hostname \n"
     exit 1
 }
 
 save_from_backup() {
-    echo "Mise en maintenance du serveur NextCloud"
+    echo "Mise en maintenance du serveur NextCloud \n"
     ssh -p $port $host "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --on"
 
-    echo "Sauvegarde des fichiers du serveur NextCloud"
+    echo "Sauvegarde des fichiers du serveur NextCloud \n"
     rsync -Aavx -e "ssh -p $port" $host:/var/www/html/nextcloud/ /data/backup/nextcloud-dirbackup/
 
-    echo "Dump de la base de données NextCloud"
+    echo "Dump de la base de données NextCloud \n"
     ssh -p $port $host "mysqldump --single-transaction -h $DATABASE_HOST -u $DATABASE_USER --password=$DATABASE_PASSWORD $DATABASE_DBNAME" > /data/backup/nextcloud-dbbackup.bak
 
-    echo "Archivage du dossier backup"
+    echo "Archivage du dossier backup \n"
     zfs snapshot data/backup@nextcloud_`date +"%Y%m%d%H%M"`
 
     # Retention du nombre de snapshot (limité à 30 jours)
@@ -36,7 +36,7 @@ save_from_backup() {
     done
 
     ssh -p $port $host "sudo -u www-data php /var/www/html/nextcloud/occ maintenance:mode --off"
-    echo "Le serveur NextCloud est de nouveau opérationnel"
+    echo "Le serveur NextCloud est de nouveau opérationnel \n"
 
     exit 0
 }
